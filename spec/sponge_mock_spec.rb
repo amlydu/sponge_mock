79455c086deca866615e111a247e633b5f8ac4f3
Amy <amlydu@umich.edu>
2019-01-02 15:55:25 -0700

    Brief specs.

--- spec/sponge_mock_spec.rb
+++ spec/sponge_mock_spec.rb
@@ -1,9 +1,13 @@
 RSpec.describe SpongeMock do
-  it "has a version number" do
+  it 'has a version number' do
     expect(SpongeMock::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'camelcases strings in a mocking way' do
+    expect('Stop mocking me!'.to_sponge_mock).to eq('StOp mOcKiNg mE!')
+  end
+
+  it "returns: 'nice try you need to input a string' in a mocking way if you enter a blank string" do
+    expect(''.to_sponge_mock).to eq('NiCe tRy yOu NeEd To iNpUt A StRiNg')
   end
 end
